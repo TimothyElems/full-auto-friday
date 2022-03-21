8baae5e8ec4cab817b8b2d3db00f46543f91b6f6
TimothyElems <85181892+TimothyElems@users.noreply.github.com>
2022-03-21 23:12:21 +0100

    Initial Commit
    Spacefiller. Nothing done yet, I just need to build consistency in posting on the hub.

---
+++ Pattern Matching with Regular Expressions/without_reg_ex.py
@@ -0,0 +1,28 @@
+'''
+all good programmers start with documentation
+
+This first program is showing how to find patterns without usign Regular Expressions. A sort of before and after approach to the thingy.
+'''
+
+def isPhoneNumber(text):
+	if len(text) != 12:
+		return False 
+	for i in range(0, 3):
+		if not text[i].isdecimal():
+			return False
+	if text[3] != '-':
+		return False
+	for i in range(4, 7):
+		if not text[i].isdecimal():
+			return False
+	if text [7] != '-':
+		return False
+	for i in range(8, 12):
+		if not text[i].isdecimal():
+			return False
+	return True
+	
+print('269-471-6818 is a phone number:')
+print(isPhoneNumber('269-471-6818'))
+print('Yama yama is a phone number:')
+print(isPhoneNumber('Yama yama'))
